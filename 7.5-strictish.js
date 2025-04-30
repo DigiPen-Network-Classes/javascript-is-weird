045b2ad888a968a357ac45a2602be5d3e8583a9c
Tony Rasa <trasa@meancat.com>
2025-04-30 14:08:14 -0700

    another example of javascript

--- 7.5-strictish.js
+++ 7.5-strictish.js
@@ -1,13 +1,7 @@
-"use strict"
-let x = 500;
-//delete x; // not allowed!
-
 // duplicate param name not allowed:
-/*
+
 function foo(x, x) {
     console.log(`hi ${x}?!`);
 }
-*/
+foo("a", "b");
 
-function foo(x) { return x; }
-delete foo; //  nope
